3f5d015cb968d05ee20b04276bd4d6303da031a3
zahrabateninia <zahrabatenin@gmail.com>
2023-11-08 16:24:51 +0330

    Initialize Warrior and Healer constructors and connect them to Hero

--- characterSelect/characterSelect.js
+++ characterSelect/characterSelect.js
@@ -8,3 +8,15 @@ let hero1 = new Hero('Bjorn', 1);
 Hero.prototype.greet = function () {
   return `${this.name} says hello.`;
 }
+
+function Warrior(name, level, weapon) {
+  // Chain constructor with call
+  Hero.call(this, name, level);
+  this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+  Hero.call(this, name, level);
+
+  this.spell = spell;
+}
