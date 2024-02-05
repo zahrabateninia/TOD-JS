52f2da8db2d5d0194e1a1a2a392b85e49bcf22e0
zahrabateninia <zahrabatenin@gmail.com>
2024-02-05 08:49:24 +0330

    Represent a server before the function definitions

---
+++ getInfoFromServer.js
@@ -0,0 +1,25 @@
+const server = {
+    people: [
+      {
+        name: "Odin",
+        age: 20,
+      },
+      {
+        name: "Thor",
+        age: 35,
+      },
+      {
+        name: "Freyja",
+        age: 29,
+      },
+    ],
+  
+    getPeople() {
+      return new Promise((resolve, reject) => {
+        // Simulating a delayed network call to the server
+        setTimeout(() => {
+          resolve(this.people);
+        }, 2000);
+      });
+    },
+  };
